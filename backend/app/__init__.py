3b8678a571dfef02b06885a1b65f522c2b9e8b9d
bosslibra <alessandr.libralesso@studio.unibo.it>
2026-04-28 22:33:45 +0200

    refactor: removes unused code

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,16 +5,13 @@ from flask import Flask
 
 def create_app():
     """Create the Flask app."""
-    app = Flask(__name__, static_folder='/tech_stack_tracker/static', template_folder='/tech_stack_tracker/home/templates')
+    app = Flask(__name__)
     app.config.from_object("config.Config")
 
     with app.app_context():
         # Import the parts of the application
-
-        # Page-serving blueprints
-
         # API blueprints
-        from app.api import auth, health_check
+        from app.api import health_check
 
         # Register the blueprints
         app.register_blueprint(health_check.health_check_bp)
